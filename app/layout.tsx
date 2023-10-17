47e44e0837df2ab0cb67b0f393bfdd09ae92261f
Tiny_Murky <murky0830@gmail.com>
2023-10-17 19:46:12 +0800

    chore: nextjs init with new LICENCES

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Tiny_Murky\' Blogger',
+  description: 'Blogger Created by Tiny_Murky',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className}`}>
+        {children}
+      </body>
+    </html>
+  )
+}

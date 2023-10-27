b0d9b3a74a4aa44326a0cf52f69de20599a1e24a
Tiny_Murky <murky0830@gmail.com>
2023-10-27 15:00:07 +0800

    feat: clipboard copy complete

--- components/CopyCodePreButton.tsx
+++ components/CopyCodePreButton.tsx
@@ -3,6 +3,9 @@ import React from 'react'
 import { useState, useRef, useEffect } from 'react'
 import clsx from 'clsx'
 import { GoCopy, GoCheck } from "react-icons/go"
+
+import copyToClipBoard from '@/libs/copyToClipBoard'
+
 type Props = React.ComponentPropsWithoutRef<"pre">
 
 export default function CopyCodePreButton({children, className, ...props}: Props) {
@@ -10,12 +13,17 @@ export default function CopyCodePreButton({children, className, ...props}: Props
   const [copied, setCopied] = useState(false)
 
   useEffect(() => {
-    const timer = setTimeout(() => setCopied(false), 2000)
+    const timer = setTimeout(() => setCopied(false), 800)
 
     return () => clearTimeout(timer) // 刪掉setTimeout
   }, [copied]) 
 
-  const handleClicked = () => {
+  const handleClicked = async () => {
+    const codeText = preRef.current?.querySelector('code')?.innerText
+    if (codeText){
+      // 用ref 抓到pre裡的innerText
+      await copyToClipBoard(codeText)
+    }
     setCopied(true)
   }
 
@@ -30,12 +38,12 @@ export default function CopyCodePreButton({children, className, ...props}: Props
     codeLanguage = languageClass.split('-')[1]
   }
 
-
+  // 最外層的div需要在mt-14上加important，不然有的時候prose會蓋過變成mt-0
   return (
-    <div className='group relative mt-14'>
+    <div className='group relative !mt-14 block'>
       <pre {...props} ref={preRef} className={clsx(className)}>
         <div className='absolute w-full -top-8 right-0 dark:bg-gray-700 bg-opacity-50 text-base  py-2 ps-4 rounded-t-md'>
-          {codeLanguage}
+          <span className='dark:text-gray-300'>{codeLanguage}</span>
           <div className={
             clsx('absolute top-0 right-1 mt-1 flex gap-2 items-center ',
             {
@@ -51,13 +59,13 @@ export default function CopyCodePreButton({children, className, ...props}: Props
               Copied!
             </span>
             <button onClick={handleClicked} className={clsx(
-              "rounded-md border bg-transparent p-2 transition ease-in focus:outline-none flex",
+              "rounded-md border bg-transparent p-2 transition ease-in focus:outline-none flex items-center",
               {
                 "dark:hover:border-gray-400  dark:border-gray-700 ":!copied,
                 "border-green-400": copied,
               }
             )}>
-              <GoCopy className={clsx('ease-in transition',
+              <GoCopy className={clsx('ease-in transition dark:text-gray-300',
                   {
                     "hidden": copied
                   }

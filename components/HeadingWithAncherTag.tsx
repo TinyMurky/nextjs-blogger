e9b4c9460789e7b766976f7ffcbba2108e397314
Tiny_Murky <murky0830@gmail.com>
2023-10-27 16:36:04 +0800

    feat: ancher tag complete

---
+++ components/HeadingWithAncherTag.tsx
@@ -0,0 +1,40 @@
+// 需要先在rehype rehype-slug plugin
+import React from 'react'
+import clsx from 'clsx'
+import { GoLink } from "react-icons/go"
+type Props = React.ComponentPropsWithoutRef< 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' > &
+  {Component: "h1" | "h2" | "h3" | "h4" | "h5" | "h6"} // props中的component只可以是title
+
+function HeadingWithAncherTag({Component, id ,className, children, ...otherProps}: Props) {
+  return (
+    <Component id={id} className={clsx('scroll-mt-20  group flex flex-row gap-3 items-center  justify-start', className)} {...otherProps}>
+      <span className=''>{children}</span>
+        <a
+          href={id && `#${id}`} // 如果id有值的話走後面的 #id 沒有的話就是 id
+          className='hidden group-hover:inline-flex text-base   items-center w-8 h-8 dark:text-gray-400 dark:hover:text-gray-200  dark:hover:border-gray-400  dark:border-gray-700 rounded-md border bg-transparent focus:outline-none '
+          aria-label="Anchor"
+        >
+          <GoLink className='mx-auto'/>
+        </a>
+    </Component>
+  )
+}
+
+export const AncherTagH1 = (props: React.ComponentPropsWithRef<"h1">) => (
+  <HeadingWithAncherTag Component="h1" {...props} />
+)
+export const AncherTagH2 = (props: React.ComponentPropsWithRef<"h2">) => (
+  <HeadingWithAncherTag Component="h2" {...props} />
+)
+export const AncherTagH3 = (props: React.ComponentPropsWithRef<"h3">) => (
+  <HeadingWithAncherTag Component="h3" {...props} />
+)
+export const AncherTagH4 = (props: React.ComponentPropsWithRef<"h4">) => (
+  <HeadingWithAncherTag Component="h4" {...props} />
+)
+export const AncherTagH5 = (props: React.ComponentPropsWithRef<"h5">) => (
+  <HeadingWithAncherTag Component="h5" {...props} />
+)
+export const AncherTagH6 = (props: React.ComponentPropsWithRef<"h6">) => (
+  <HeadingWithAncherTag Component="h6" {...props} />
+)
--- libs/mdxComponents.ts
+++ libs/mdxComponents.ts
@@ -1,7 +1,20 @@
 import CopyCodePreButton from "@/components/CopyCodePreButton"
-
+import { 
+  AncherTagH1,
+  AncherTagH2,
+  AncherTagH3,
+  AncherTagH4,
+  AncherTagH5,
+  AncherTagH6
+} from "@/components/HeadingWithAncherTag"
 const mdxComponents = {
   pre: CopyCodePreButton,
+  h1: AncherTagH1,
+  h2: AncherTagH2,
+  h3: AncherTagH3,
+  h4: AncherTagH4,
+  h5: AncherTagH5,
+  h6: AncherTagH6,
 }
 
 export default mdxComponents

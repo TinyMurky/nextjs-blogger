11a015eec183b0e41a312c47dfae0ca5e2d86fb5
Tiny_Murky <murky0830@gmail.com>
2023-10-28 11:35:24 +0800

    fix: img of ListItem now can be normally display on chrome

--- app/blogs/components/SearchBar.tsx
+++ app/blogs/components/SearchBar.tsx
@@ -24,7 +24,7 @@ export default function SearchBar({ search, setSearch }:Props) {
         type="text"
         value={search}
         onChange={event => setSearch(event.target.value.trim())}
-        className="bg-gray-600 focus:bg-opacity-50 outline-none caret-white/90 text-white/90 px-2 py-1 lg:p-2 w-40 lg:w-60 text-base lg:text-xl rounded-xl"
+        className="bg-gray-600 focus:bg-opacity-50 outline-none caret-white/90 text-white/90 px-2 py-1 lg:py-2 lg:px-3 w-32 sm:w-40 lg:w-60 text-base lg:text-xl rounded-xl"
         placeholder="Search by Title"
       />
       <button className="hidden lg:inline-flex p-2 text-xl rounded-xl bg-gray-500 bg-opacity-50 hover:bg-opacity-100 ml-2 font-bold">

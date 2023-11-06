bf46abe745d9c4cc64641aa6f8262dcd2eb3d069
Tiny_Murky <murky0830@gmail.com>
2023-11-06 21:45:36 +0800

    feat: prisma schema and seeder can load mdx now

---
+++ prisma/seed.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from '@prisma/client'
+import { getBlogsData } from './getBlogs'
+const prisma = new PrismaClient()
+async function main() {
+  const blogs = getBlogsData('/public/blogs', 'mdx')
+
+  await prisma.user.upsert({
+    where: { email: 'murky0830@gmail.com' },
+    update: {},
+    create: {
+      email: 'murky0830@gmail.com',
+      name: 'Tiny Murky',
+      blogs: {
+        create: blogs
+      },
+    },
+  })
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })

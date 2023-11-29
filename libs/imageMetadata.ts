1f04d9eb5eedcfa88671873f7da028657c5647c8
Tiny_Murky <murky0830@gmail.com>
2023-11-29 21:31:28 +0800

    remove unused function

--- libs/imageMetadata.ts
+++ libs/imageMetadata.ts
@@ -1,17 +1,13 @@
 // ref https://easonchang.com/posts/post-custom-image
- // restucture img src and get with and hight
-import imageSize from "image-size"
 import path from "path";
 import { getPlaiceholder } from "plaiceholder"
 import { visit } from "unist-util-visit"
 // @ts-ignore
 import type { Node } from "unist-util-visit"
-import { promisify } from "util"
 import fs from "node:fs/promises"
 import { removeCwdUrl } from './removeRepeatUrl'
 import https from 'https'
-//use promise
-const sizeOf = promisify(imageSize)
+
 
 // def image interface
 interface ImageNode extends Node {
@@ -73,25 +69,30 @@ async function addMetadata(node: ImageNode): Promise<void> {
 
   let buffer: Buffer
   if (node.properties.src.startsWith('http')){
+
     buffer = await downloadImage(node.properties.src)
 
   } else {
+
     const absoluteNodeSrc = path.join(process.cwd(), "public", node.properties.src)
     buffer = await fs.readFile(absoluteNodeSrc)
+
   }
 
 
   try{
-    const { metadata: { height, width }, base64 } = await getPlaiceholder(buffer, { size: 10 })// 10 is to increase detail (default is 4)
 
+    const { metadata: { height, width }, base64 } = await getPlaiceholder(buffer, { size: 10 })// 10 is to increase detail (default is 4)
 
     node.properties.width = width
     node.properties.height = height
     node.properties.base64 = base64
+
   }catch(error){
     throw Error(`Invalid image with src "${node.properties.src}"`)
   }
 }
+
 
 export default function imageMetadata() {
   return async function transformer(tree: Node, file:any): Promise<Node> {
@@ -120,17 +121,3 @@ export default function imageMetadata() {
   }
 }
 
-
-export function transformImgSrc(fileFolder: string) {
-  return (tree: any, file: any) => {
-    // 找到img元素，然後更改它的src屬性
-    fileFolder = removeCwdUrl(file.path)
-    visit(tree, 'element', node => {
-      if (node.tagName === 'img') {
-        const src = node.properties.src as string
-        const fileName = src.replace('./', '')
-        node.properties.src = `${fileFolder}/${fileName}`
-      }
-    })
-  }
-}

868b09fd08b4fc29826ceff76e965a7a0082da3d
Tiny_Murky <murky0830@gmail.com>
2023-12-15 17:12:35 +0800

    fix sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next-sitemap').IConfig} */
 
+const sitemap = process.env.NEXT_PUBLIC_API_URL || 'https://nextjs-blogger-production.up.railway.app'
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
+  siteUrl: sitemap,
   generateRobotsTxt: true,
   generateIndexSitemap: false, // 給更大網佔用的
   sitemapSize: 7000,
   exclude: ['/server-sitemap.xml'], // <= for dynamic
   robotsTxtOptions: {
     additionalSitemaps: [
-      `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'}/server-sitemap.xml`, // <==== Add here
+      `${sitemap || 'http://localhost:3000'}/server-sitemap.xml`, // <==== Add here
     ],
   },
 }
